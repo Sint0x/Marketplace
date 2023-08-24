7114c84ac5d19e0c959545387dc4beadede694d0
Tech7ie <madexlol6661@gmail.com>
2023-08-25 02:52:46 +1000

    general code readability improvements

--- frontend/src/components/routes/Routing.js
+++ frontend/src/components/routes/Routing.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Auth from "../auth/Auth"; // в данном импорте Auth является компонентом, импортированым из файла по пути.
-                                  // Auth - название функции в том файле, возвращиющий компонент
+import Auth from "../auth/Auth";
 import { Routes, Route } from 'react-router-dom';
 import MainPage from "../MainPage/Main";
 import Profile from "../Profile/Profile";
 import PrivateRoute from "../routes/PrivateRoute";
 import AnonymousRoute from "../routes/AnonymousRoute";
 import Loggedout from "../auth/Loggedout";
-// в этой функции обьявлены маршруты, использующиеся в App.js при помощи export default(импорта)
-// element={<Login />} рендерит в index.html элемент Login
+
+
+
 export default function MembersRouter() {
   return (
       <Routes>
--- projjj/urls.py
+++ projjj/urls.py
@@ -1,15 +1,9 @@
-from projjjapi import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/testcontent', views.goods, name='my_view'),
-    path('api/register/', views.register, name='api_register'),
-    path('api/login', views.login, name='login'),
-    path('api/tokencheck', views.token_check, name='token_check'),
-    path('api/profile', views.userData, name='create_user'),
-    path('api/testoken', views.ifLogin, name='testoken'),
+    path('api/', include('projjjapi.urls')),
 ]
 
