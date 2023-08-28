7f4a0ca8e7082b5b8787c8c75fe904a1b836ca53
Tech7ie <madexlol6661@gmail.com>
2023-08-28 21:13:51 +1000

    Delete

--- frontend/src/components/Goods/script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  let Filters = document.getElementById("Filters");
-  if (Filters.style.display === "none") {
-    Filters.style.display = "block";
-  } else {
-    Filters.style.display = "none";
-  }
-}
--- frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-// рендерит элементы из App.js в index.html по id root
-ReactDOM.render(<App />, document.getElementById('root'));
