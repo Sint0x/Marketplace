b4a7ca935ed34b123a07ce837f28838ed27e4104
Tech7ie <madexlol6661@gmail.com>
2023-08-29 22:28:49 +1000

    auth form style fix

--- frontend/src/components/auth/Auth.js
+++ frontend/src/components/auth/Auth.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './style.css';
 import LoginForm from './Login';
 import RegistrationForm from './Registr';
+import Header from '../Elements/Header/Header';
 
 
 // страница авторизации\регистрации
@@ -18,52 +19,33 @@ const Auth = () => {
     setShowLogin(false);
     setShowRegistration(true);
   };
-  React.useEffect(() => {
-    document.body.style.fontFamily = "'Lato', sans-serif";
-    document.body.style.margin = 0;
-    document.body.style.minHeight = '100vh';
-    document.body.style.display = 'flex';
-    document.body.style.justifyContent = 'center';
-    document.body.style.alignItems = 'center';
-    document.body.style.backgroundColor = 'rgb(56, 87, 143)';
-    document.body.style.backgroundBlendMode = 'difference';
-    document.body.style.transition = '0s';
-
-    return () => {
-      document.body.style.fontFamily = null;
-      document.body.style.margin = null;
-      document.body.style.minHeight = null;
-      document.body.style.display = null;
-      document.body.style.justifyContent = null;
-      document.body.style.alignItems = null;
-      document.body.style.backgroundColor = null;
-      document.body.style.backgroundBlendMode = null;
-      document.body.style.transition = null;
-    };
-  }, []);
-
   return (
-    <div className="mainblock">
-      <div className="block">
-        <div className={`blockhidden formlogin ${showLogin ? 'b-showlogin' : ''}`}>
-          <div>
-            <LoginForm />
+    <>
+      <Header />
+      <div className="center" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '88vh' }}>
+        <div className="mainblock">
+          <div className="block">
+            <div className={`blockhidden formlogin ${showLogin ? 'b-showlogin' : ''}`}>
+              <div>
+                <LoginForm />
+              </div>
+            </div>
+            <div className={`blockhiddenreg formreg ${showRegistration ? 'b-showreg' : ''}`} id="Registration">
+              <RegistrationForm />
+            </div>
+
+            <section className="block__item block-item">
+              <h2 className="phrase">У вас уже есть аккаунт?</h2>
+              <button className="button" onClick={handleLoginClick}>Войти</button>
+            </section>
+            <section className="block__item block-item">
+              <h2 className="phrasereg">У вас нет аккаунта?</h2>
+              <button className="buttonreg" onClick={handleRegistrationClick}>Регистрация</button>
+            </section>
           </div>
         </div>
-        <div className={`blockhiddenreg formreg ${showRegistration ? 'b-showreg' : ''}`} id="Registration">
-          <RegistrationForm />
-        </div>
-
-        <section className="block__item block-item">
-          <h2 className="phrase">У вас уже есть аккаунт?</h2>
-          <button className="button" onClick={handleLoginClick}>Войти</button>
-        </section>
-        <section className="block__item block-item">
-          <h2 className="phrasereg">У вас нет аккаунта?</h2>
-          <button className="buttonreg" onClick={handleRegistrationClick}>Регистрация</button>
-        </section>
       </div>
-    </div>
+    </>
   );
 };
 
