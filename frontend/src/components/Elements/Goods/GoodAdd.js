b5dae8e7ed80180d637bb46cfabb260847ef5961
Tech7ie <madexlol6661@gmail.com>
2023-09-01 01:33:48 +1000

    null image fix

--- frontend/src/components/Elements/Goods/GoodAdd.js
+++ frontend/src/components/Elements/Goods/GoodAdd.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
 import Header from '../Header/Header';
+
 function AddGoodForm() {
     const [descriptionGoods, setDescriptionGoods] = useState('');
     const [price, setPrice] = useState('');
     const [images, setImages] = useState(null);
     const [namegoods, setNamegoods] = useState('');
     const [afrom, setAfrom] = useState('');
+    const [error, setError] = useState(null);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
 
+        if (!images) {
+            setError('Изображение обязательно');
+            return;
+        }
+
+        setError(null);
         const token = localStorage.getItem("key");
         const formData = new FormData();
         formData.append('description_goods', descriptionGoods);
@@ -35,49 +43,50 @@ function AddGoodForm() {
 
     return (
         <>
-        <Header />
-        <form onSubmit={handleSubmit}>
-            <label>
-                Описание товара:
-                <input
-                    type="text"
-                    value={descriptionGoods}
-                    onChange={event => setDescriptionGoods(event.target.value)}
-                />
-            </label><br />
-            <label>
-                Цена:
-                <input
-                    type="text"
-                    value={price}
-                    onChange={event => setPrice(event.target.value)}
-                />
-            </label><br />
-            <label>
-                Изображение:
-                <input
-                    type="file"
-                    onChange={event => setImages(event.target.files[0])}
-                />
-            </label><br />
-            <label>
-                Название товара:
-                <input
-                    type="text"
-                    value={namegoods}
-                    onChange={event => setNamegoods(event.target.value)}
-                />
-            </label><br />
-            <label>
-                Место происхождения:
-                <input
-                    type="text"
-                    value={afrom}
-                    onChange={event => setAfrom(event.target.value)}
-                />
-            </label><br />
-            <button type="submit">Добавить товар</button>
-        </form>
+            <Header />
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Описание товара:
+                    <input
+                        type="text"
+                        value={descriptionGoods}
+                        onChange={event => setDescriptionGoods(event.target.value)}
+                    />
+                </label><br />
+                <label>
+                    Цена:
+                    <input
+                        type="text"
+                        value={price}
+                        onChange={event => setPrice(event.target.value)}
+                    />
+                </label><br />
+                <label>
+                    Изображение:
+                    <input
+                        type="file"
+                        onChange={event => setImages(event.target.files[0])}
+                    />
+                </label><br />
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+                <label>
+                    Название товара:
+                    <input
+                        type="text"
+                        value={namegoods}
+                        onChange={event => setNamegoods(event.target.value)}
+                    />
+                </label><br />
+                <label>
+                    Место происхождения:
+                    <input
+                        type="text"
+                        value={afrom}
+                        onChange={event => setAfrom(event.target.value)}
+                    />
+                </label><br />
+                <button type="submit">Добавить товар</button>
+            </form>
         </>
     );
 }
--- frontend/src/components/Elements/Goods/GoodsCard.js
+++ frontend/src/components/Elements/Goods/GoodsCard.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './style.css';
 
-
-// попытка реализации карточки товара
 const GoodsCard = () => {
   const [products, setProducts] = useState([]);
   const filtersRef = useRef(null);
@@ -30,31 +28,34 @@ const GoodsCard = () => {
   }
 
   return (
-      <div className='card'>
-        {products.map((product) => {
-          const { username, rating, first_name, last_name, description_goods, price, images, namegoods, goods_id, afrom } = product;
+    <div className='card'>
+      {products.map((product) => {
+        const { username, rating, first_name, last_name, description_goods, price, images, namegoods, goods_id, afrom } = product;
+        let myImage;
+        if (images) {
           const imageName = images.split('/').pop();
-          const myImage = require(`../../../goods/images/${imageName}`);
-          return (
-            <div key={goods_id} className="product-card">
-              <div className="product-image">
-                <img className="image" src={myImage} alt={namegoods} />
-              </div>
-              <div className="product-info">
-                <div className="space">
-                  <h1 className="good_name">{namegoods.substring(0, 75)}</h1>
-                  <h3 className="good_price">Цена: {price} руб.</h3>
-                  <div className="discripto" >{description_goods.substring(0, 200)}</div>
-                </div>  
-              <div className="adress"><p className="good_adress">{afrom}</p><p className="rating_seller">Продавец: {rating}⭐<br></br><span style={{ marginRight: '10px' }}>{first_name}</span><span style={{ marginRight: '10px' }}>{username}</span>{last_name}</p></div>
-              </div>
+          myImage = require(`../../../goods/images/${imageName}`);
+        } else {
+          myImage = require(`../../../goods/images/IMG_20230804_134452_081.jpg`);
+        }
+        return (
+          <div key={goods_id} className="product-card">
+            <div className="product-image">
+              {myImage && <img className="image" src={myImage} alt={namegoods} />}
             </div>
-          );
-        })}
-      </div>
+            <div className="product-info">
+              <div className="space">
+                <h1 className="good_name">{namegoods.substring(0, 75)}</h1>
+                <h3 className="good_price">Цена: {price} руб.</h3>
+                <div className="discripto" >{description_goods.substring(0, 200)}</div>
+              </div>
+              <div className="adress"><p className="good_adress">{afrom}</p><p className="rating_seller">Продавец: {rating}⭐<br></br><span style={{ marginRight: '10px' }}>{first_name}</span><span style={{ marginRight: '10px' }}>{username}</span>{last_name}</p></div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 };
-
-
 
 export default GoodsCard;
